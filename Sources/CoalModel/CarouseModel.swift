006ab120e809a95e8895cf8b2fa8209d1067375f
ArifRach <Arifrach31@gmail.com>
2024-09-20 15:32:32 +0700

    fix carousel view

--- Sources/CoalModel/CarouseModel.swift
+++ Sources/CoalModel/CarouseModel.swift
@@ -1,5 +1,5 @@
 //
-//  CarouselItemModel.swift
+//  CarouselModel.swift
 //
 //
 //  Created by ArifRachman on 19/09/24.
@@ -12,12 +12,14 @@ public struct CarouselModel: Identifiable {
   public var color: Color
   public var image: String
   public var page: CardPage
+  public var url: String
   
-  public init(id: Int, color: Color, page: CardPage, image: String = "") {
+  public init(id: Int, color: Color, page: CardPage, image: String = "", url: String = "") {
     self.id = id
     self.color = color
     self.page = page
     self.image = image
+    self.url = url
   }
 }
 
@@ -25,4 +27,5 @@ public enum CardPage: String {
   case pageOne = "Page One"
   case pageTwo = "Page Two"
   case pageThree = "Page Three"
+  case pageFour = "Page Four"
 }
