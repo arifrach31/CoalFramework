ba5240a699a7b33e571bb8aa06b6b3c4757a972b
ArifRach <Arifrach31@gmail.com>
2024-10-04 11:07:33 +0700

    Add InitCoalConfig for CoalFramework

---
+++ Sources/CoalCore/Config/LoginConfig.swift
@@ -0,0 +1,12 @@
+//
+//  LoginConfig.swift
+//  
+//
+//  Created by ArifRachman on 04/10/24.
+//
+
+import Foundation
+
+public class LoginConfig {
+  
+}
---
+++ Sources/CoalCore/Config/RegisterConfig.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterConfig.swift
+//
+//
+//  Created by ArifRachman on 04/10/24.
+//
+
+import Foundation
+
+public class RegisterConfig {
+  
+}
