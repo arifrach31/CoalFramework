a89977da11f276262bfdc7c1bce043d2d3e7fb02
ArifRach <Arifrach31@gmail.com>
2024-11-04 13:29:40 +0700

    Fix AppProvider Client

--- Sources/CoalTemplate/Config/AppProvider.swift
+++ Sources/CoalTemplate/Config/AppProvider.swift
@@ -1,60 +1,55 @@
 //
-//  CoalConfig.swift
+//  AppProvider.swift
+//  CoalClient
 //
-//
-//  Created by ArifRachman on 04/10/24.
+//  Created by ArifRachman on 05/10/24.
 //
 
+import CoalFramework
 import CoalCore
 
-public protocol CoalConfigDelegate {
-  func initCoalConfig() -> CoalConfig?
-}
-
-public protocol CoalAppProvider {
-  var splashProvider: SplashConfigProvider? { get }
-  var loginProvider: LoginConfigProvider? { get }
-  var registerProvider: RegisterConfigProvider? { get }
-  var verificationProvider: VerificationConfigProvider? { get }
-  var menuProvider: MenuConfigProvider? { get }
-  var homeProvider: HomeConfigProvider? { get }
-  var networkProvider: NetworkConfigProvider? { get }
-  var forgotProvider: ForgotConfigProvider? { get }
-  var changePasswordProvider: ChangePasswordConfigProvider? { get }
+class AppProvider: CoalAppProvider {
+  let splashProvider: SplashConfigProvider?
+  let loginProvider: LoginConfigProvider?
+  let registerProvider: RegisterConfigProvider?
+  var verificationProvider: VerificationConfigProvider?
+  let menuProvider: MenuConfigProvider?
+  let homeProvider: HomeConfigProvider?
+  let networkProvider: NetworkConfigProvider?
+  var forgotProvider: ForgotConfigProvider?
+  var changePasswordProvider: ChangePasswordConfigProvider?
   
-  func getConfig() -> CoalConfig?
-}
-
-public struct CoalConfig {
-  public static let shared = CoalConfig()
+  init(splashProvider: SplashConfigProvider? = nil,
+       loginProvider: LoginConfigProvider? = nil,
+       registerProvider: RegisterConfigProvider? = nil,
+       verificationProvider: VerificationConfigProvider? = nil,
+       menuProvider: MenuConfigProvider? = nil,
+       homeProvider: HomeConfigProvider? = nil,
+       networkProvider: NetworkConfigProvider? = nil,
+       forgotProvider: ForgotConfigProvider? = nil,
+       changePasswordProvider: ChangePasswordConfigProvider? = nil) {
+    self.splashProvider = splashProvider
+    self.loginProvider = loginProvider
+    self.registerProvider = registerProvider
+    self.verificationProvider = verificationProvider
+    self.menuProvider = menuProvider
+    self.homeProvider = homeProvider
+    self.networkProvider = networkProvider
+    self.forgotProvider = forgotProvider
+    self.changePasswordProvider = changePasswordProvider
+  }
   
-  public let splashConfig: SplashConfig?
-  public let loginConfig: LoginConfig?
-  public let registerConfig: RegisterConfig?
-  public let verificationConfig: VerificationConfig?
-  public let menuConfig: MenuConfig?
-  public let homeConfig: HomeConfig?
-  public let networkConfig: NetworkConfig?
-  public let forgotConfig: ForgotConfig?
-  public let changePasswordConfig: ChangePasswordConfig?
-  
-  public init(splashConfig: SplashConfig? = nil,
-              loginConfig: LoginConfig? = nil,
-              registerConfig: RegisterConfig? = nil,
-              verificationConfig: VerificationConfig? = nil,
-              menuConfig: MenuConfig? = nil,
-              homeConfig: HomeConfig? = nil,
-              networkConfig: NetworkConfig? = nil,
-              forgotConfig: ForgotConfig? = nil,
-              changePasswordConfig: ChangePasswordConfig? = nil) {
-    self.splashConfig = splashConfig
-    self.loginConfig = loginConfig
-    self.registerConfig = registerConfig
-    self.verificationConfig = verificationConfig
-    self.menuConfig = menuConfig
-    self.homeConfig = homeConfig
-    self.networkConfig = networkConfig
-    self.forgotConfig = forgotConfig
-    self.changePasswordConfig = changePasswordConfig
+  func getConfig() -> CoalConfig? {
+    return CoalConfig(
+      splashConfig: splashProvider?.getConfig(),
+      loginConfig: loginProvider?.getConfig(),
+      registerConfig: registerProvider?.getConfig(),
+      verificationConfig: verificationProvider?.getConfig(),
+      menuConfig: menuProvider?.getConfig(),
+      homeConfig: homeProvider?.getConfig(),
+      networkConfig: networkProvider?.getConfig(),
+      forgotConfig: forgotProvider?.getConfig(),
+      changePasswordConfig: changePasswordProvider?.getConfig()
+    )
   }
 }
